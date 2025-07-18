f5a49f416a663800c3193781eeccf2e3915b500c
Denis Tarasenko <deniskatarasenko6@gmail.com>
2025-07-18 14:37:02 +0300

    feat(Spend5Minutes): Add mobile-specific bottom padding
      - Added `max-sm:pb-16` to the Spend5Minutes component to increase bottom padding on smaller screens.

--- components/Spend5Minutes.tsx
+++ components/Spend5Minutes.tsx
@@ -3,7 +3,7 @@
 function Spend5Minutes() {
   return (
     <div
-      className={`flex flex-col leading-[48px] xl:leading-[68px] text-[24px] xl:text-[40px] text-gray font-[500] gap-6 xl:gap-10 max-w-[764px] mx-auto py-6 md:py-20 transition-all duration-700`}
+      className={`flex flex-col leading-[48px] xl:leading-[68px] text-[24px] xl:text-[40px] text-gray font-[500] gap-6 xl:gap-10 max-w-[764px] mx-auto py-6 max-sm:pb-16 md:py-20 transition-all duration-700`}
     >
       <div>
         Setup your cross-platform mobile app in just{" "}
