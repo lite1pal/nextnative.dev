ae70ba588e4ade62ab4666310535ce1631d70b8f
Denis Tarasenko <deniskatarasenko6@gmail.com>
2025-08-10 20:29:15 +0300

    feat(sitemap): Include documentation pages in sitemap
    - Add static URLs for documentation pages to the sitemap.
    - Update the sitemap generation to include these URLs.
    - Ensure proper indexing of documentation pages by search engines.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,6 +9,25 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     select: { slug: true, updatedAt: true },
   });
 
+  const docs = [
+    "https://docs.nextnative.dev",
+    "https://docs.nextnative.dev/tutorials/ship-in-5-minutes",
+    "https://docs.nextnative.dev/tutorials/ionic-router",
+    "https://docs.nextnative.dev/tutorials/native-device-features",
+    "https://docs.nextnative.dev/tutorials/splash-screen",
+    "https://docs.nextnative.dev/tutorials/onboarding-flow",
+    "https://docs.nextnative.dev/features/database",
+    "https://docs.nextnative.dev/features/nextjs-api",
+    "https://docs.nextnative.dev/features/authorization",
+    "https://docs.nextnative.dev/features/in-app-purchases",
+    "https://docs.nextnative.dev/features/push-notifications",
+    "https://docs.nextnative.dev/preparing-for-deployment/rename-your-app",
+    "https://docs.nextnative.dev/preparing-for-deployment/change-app-icon",
+    "https://docs.nextnative.dev/preparing-for-deployment/build-app-for-production",
+    "https://docs.nextnative.dev/components/screen-container",
+    "https://docs.nextnative.dev/components/modal",
+  ];
+
   return [
     {
       url: "https://nextnative.dev/",
@@ -30,6 +49,11 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: "https://nextnative.dev/pricing",
       lastModified: new Date(),
     },
+
+    ...docs.map((doc) => ({
+      url: doc,
+      lastModified: new Date(),
+    })),
 
     ...posts.map((post) => ({
       url: `https://nextnative.dev/blog/${post.slug}`,
