9493bcdf9eb6c2ae2e61a6a332f8d4ea5b890b5a
Denis Tarasenko <deniskatarasenko6@gmail.com>
2025-09-19 19:06:07 +0200

    feat(footer): Add LinkedIn icon to footer
    - Added LinkedIn icon with link to profile
    - Added tracking event for LinkedIn icon click

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -111,6 +111,31 @@ function Footer() {
                   />
                 </svg>
               </a>
+              <a
+                aria-label="View LinkedIn profile"
+                onClick={() => trackEvent("Footer_LinkedIn_clicked")}
+                href="https://www.linkedin.com/in/denis-tarasenko-nextnative/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray hover:text-foreground transition-colors"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="lucide lucide-linkedin-icon lucide-linkedin"
+                >
+                  <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+                  <rect width="4" height="12" x="2" y="9" />
+                  <circle cx="4" cy="4" r="2" />
+                </svg>
+              </a>
             </div>
           </div>
 
