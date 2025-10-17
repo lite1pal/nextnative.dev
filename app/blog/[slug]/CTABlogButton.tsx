ad379e28946e3bf5469a1e866f2600e2afc57764
Denis Tarasenko <deniskatarasenko6@gmail.com>
2025-10-17 12:44:02 +0200

    style(blog): Improve CTA button styling
    - Increase border weight to 2px
    - Increase padding and font size for larger screens

--- app/blog/[slug]/CTABlogButton.tsx
+++ app/blog/[slug]/CTABlogButton.tsx
@@ -10,7 +10,7 @@ function CTABlogButton({ post }: { post: { slug: string } }) {
         trackEvent(`BlogPostCTA_${post.slug}_clicked`);
       }}
       href="/playground"
-      className="bg-primary hover:text-primary border-primary mt-4 inline-block rounded-lg border px-4 py-2 font-medium text-white transition-colors hover:bg-white"
+      className="bg-primary hover:text-primary border-primary mt-4 inline-block rounded-lg border-2 px-4 py-2 font-medium text-white transition-colors hover:bg-white lg:px-16 lg:py-3 lg:text-xl"
       data-fast-goal={`extra_try_free_cta_clicked`}
     >
       Try for Free
