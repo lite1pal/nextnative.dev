949b17abf1f1b39a23077f7e5bd3258654d46906
Denis Tarasenko <deniskatarasenko6@gmail.com>
2025-11-30 13:02:56 +0100

    feat(social-proof): Update social proof logos and layout
    - Replace Product Hunt and Reddit logos with optimized webp versions
    - Adjust flex layouts for improved responsiveness and alignment across screen sizes
    - Update alt text for Product Hunt and Reddit Images

--- components/SocialProof.tsx
+++ components/SocialProof.tsx
@@ -1,51 +1,52 @@
 import Image from "next/image";
 import HorizontalLine from "./HorizontalLine";
+
 function SocialProof() {
   return (
-    <div className="flex md:items-center flex-col md:flex-row gap-12 py-6 max-sm:mt-16 md:py-28">
-      <div className="flex flex-col w-[200px] gap-4">
+    <div className="flex flex-col gap-12 py-6 max-sm:mt-16 md:flex-row md:items-center md:py-28">
+      <div className="flex w-[200px] flex-col gap-4">
         <p className="text-[28px] font-[500]">Featured on</p>
         <HorizontalLine className="w-[94px]" />
       </div>
 
-      <div className="flex items-center w-full justify-evenly gap-16">
-        <div className="flex items-center gap-2 shrink-0">
+      <div className="flex w-full items-center justify-evenly gap-16">
+        <div className="flex shrink-0 items-center gap-2">
           <Image
-            src="/featured-on/producthunt.png"
+            src="/featured-on/producthunt.webp"
             alt="Product Hunt"
             width={40}
             height={40}
-            className="h-12 md:h-16 w-12 md:w-16"
+            className="h-12 w-12 md:h-16 md:w-16"
           />
         </div>
 
-        <div className="flex items-center gap-2 shrink-0">
+        <div className="flex shrink-0 items-center gap-2">
           <Image
             src="/featured-on/microlaunch.svg"
             alt="Microlaunch"
             width={40}
             height={40}
-            className="h-12 md:h-16 w-12 md:w-16"
+            className="h-12 w-12 md:h-16 md:w-16"
           />
         </div>
 
-        <div className="hidden sm:flex items-center gap-2 shrink-0">
+        <div className="hidden shrink-0 items-center gap-2 sm:flex">
           <Image
             src="/x-logo.svg"
             alt="X (formerly Twitter)"
             width={40}
             height={40}
-            className="h-12 md:h-16 w-12 md:w-16"
+            className="h-12 w-12 md:h-16 md:w-16"
           />
         </div>
 
-        <div className="flex items-center gap-2 shrink-0">
+        <div className="flex shrink-0 items-center gap-2">
           <Image
-            src="/featured-on/reddit.png"
+            src="/featured-on/reddit.webp"
             alt="Reddit"
             width={40}
             height={40}
-            className="h-12 md:h-16 w-12 md:w-16"
+            className="h-12 w-12 md:h-16 md:w-16"
           />
         </div>
       </div>
